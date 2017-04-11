6cb3d56a685a9ed9161d01f7e8706a211b382e80
sausageRoll <drozdovigor1996@gmail.com>
2017-04-12 00:09:22 +0300

    Remove files

--- src/main/java/optimization/cooridate/descent/CoordinateDescent.java
+++ src/main/java/optimization/cooridate/descent/CoordinateDescent.java
@@ -4,8 +4,6 @@ package optimization.cooridate.descent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Function;
 
 /**
